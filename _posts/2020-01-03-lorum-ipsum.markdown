67df89ddc0f92e5a8cf8be3665b9afb23d641e95
beedcode <empty>
2020-01-10 22:43:30 +0100

    #fixed search and added an avatar

--- _posts/2020-01-03-lorum-ipsum.markdown
+++ _posts/2020-01-03-lorum-ipsum.markdown
@@ -2,7 +2,7 @@
 layout: post
 title:  "Lorem ipsum"
 date:   2020-01-03 20:23:37 +0100
-categories: jekyll update
+categories: lorem ipsum
 ---
 
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,35 +1,35 @@
 {
-  const SEARCH_BOX_ID = "search-box";
-  const NO_RESULTS_MESSAGE_ID = "not-found";
-  const SEARCH_RESULTS_CONTAINER_ID = "search-results";
-  const QUERY_VARIABLE_URL_STRING = "query";
+    const SEARCH_BOX_ID = "search-box";
+    const NO_RESULTS_MESSAGE_ID = "not-found";
+    const SEARCH_RESULTS_CONTAINER_ID = "search-results";
+    const QUERY_VARIABLE_URL_STRING = "query";
 
-  const extractUrlQueryParameter = (fallback = '') => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const queryParameter = urlSearchParams.get(QUERY_VARIABLE_URL_STRING);
-    return queryParameter === null ? fallback : queryParameter;
-  }
+    const extractUrlQueryParameter = (fallback = '') => {
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const queryParameter = urlSearchParams.get(QUERY_VARIABLE_URL_STRING);
+        return queryParameter === null ? fallback : queryParameter;
+    }
 
-  const setSearchBoxValue = (searchBoxValue) => {
-    document
-      .getElementById(SEARCH_BOX_ID)
-      .setAttribute("value", searchBoxValue);
-  }
+    const setSearchBoxValue = (searchBoxValue) => {
+        document
+            .getElementById(SEARCH_BOX_ID)
+            .setAttribute("value", searchBoxValue);
+    }
 
-  const showNoResultsMessage = () => {
-    document
-      .getElementById(NO_RESULTS_MESSAGE_ID)
-      .style
-      .display = "block";
-  }
+    const showNoResultsMessage = () => {
+        document
+            .getElementById(NO_RESULTS_MESSAGE_ID)
+            .style
+            .display = "block";
+    }
 
-  const setSearchResultsHtml = (innerHtml) => {
-    document
-      .getElementById(SEARCH_RESULTS_CONTAINER_ID)
-      .innerHTML = innerHtml;
-  }
+    const setSearchResultsHtml = (innerHtml) => {
+        document
+            .getElementById(SEARCH_RESULTS_CONTAINER_ID)
+            .innerHTML = innerHtml;
+    }
 
-  const createPostListingHtml = (postItem) => `
+    const createPostListingHtml = (postItem) => `
     <h2>
       <a class='search-link' href='${postItem.url}'>${postItem.title}</a>
     </h2>
@@ -41,43 +41,52 @@
     <p>
       ${postItem.content.substring(0, 150)}...
     </p>
+    
+    <div class="excerpt">
+        <a class="button" href='${postItem.url}'>
+            Anzeigen
+        </a>
+    </div>
+    <div class="post-teaser-separator">
+    </div>
   `;
 
-  const displaySearchResults = (results) => {
-    setSearchResultsHtml(
-      results
-        .map(result => createPostListingHtml(window.store[result.ref]))
-        .join('')
-    );
-  }
+    const displaySearchResults = (results) => {
+        setSearchResultsHtml(
+            results
+                .map(result => createPostListingHtml(window.store[result.ref]))
+                .join('')
+        );
+    }
 
-  const buildLunrIndex = () => {
-    return lunr(function () {
-      this.field("id");
-      this.field("title", { boost: 10 });
-      this.field("author");
-      this.field("category");
-      this.field("content");
-      for (let key in window.store) {
-        const postJson = window.store[key];
-        this.add({
-          "id": key,
-          "title": postJson.title,
-          "author": postJson.author,
-          "category": postJson.category,
-          "content": postJson.content
+    const buildLunrIndex = () => {
+        return lunr(function () {
+            this.field("id");
+            this.field("title", {boost: 10});
+            this.field("author");
+            this.field("category");
+            this.field("content");
+            for (let key in window.store) {
+                const postJson = window.store[key];
+                this.add({
+                    "id": key,
+                    "title": postJson.title,
+                    "author": postJson.author,
+                    "category": postJson.category,
+                    "content": postJson.content
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  const searchFromUrl = () => {
-    const searchTerm = extractUrlQueryParameter();
-    setSearchBoxValue(searchTerm);
-    const lunrIndex = buildLunrIndex();
-    const results = lunrIndex.search(searchTerm);
-    results.length === 0 ? showNoResultsMessage() : displaySearchResults(results);
-  }
+    const searchFromUrl = () => {
+        const searchTerm = extractUrlQueryParameter();
+        setSearchBoxValue(searchTerm);
+        const lunrIndex = buildLunrIndex();
+        const results = lunrIndex.search(searchTerm);
+        results.length === 0 ? showNoResultsMessage() : displaySearchResults(results);
+    }
 
-  searchFromUrl();
-};
+    searchFromUrl();
+}
+;
